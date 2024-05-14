2ba18302ef5b29484a445d741c77f7553380a5f7
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-14 11:55:20 +0100

    Updated the abut page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,22 @@
+import AboutJson from "@/data/about.json";
+
+export const metadata = {
+  title: "About",
+};
+
+export default function About() {
+  return (
+    <>
+      <main className="mainComponent">
+        {AboutJson.map((section, index) => (
+          <div key={index} className="mb-8">
+            <h2 className="text-3xl font-bold text-white mb-4">
+              {section.sectionTitle}
+            </h2>
+            <p className="text-gray-400">{section.sectionDesc}</p>
+          </div>
+        ))}
+      </main>
+    </>
+  );
+}
