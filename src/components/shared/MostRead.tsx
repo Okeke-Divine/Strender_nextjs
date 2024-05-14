fc9ef6a5292ab1cbd074fecfa95df8a110575770
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-14 21:25:26 +0100

    Updated

---
+++ src/components/shared/MostRead.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { useState, useEffect } from "react";
+import ComponentTitle from "./ComponentTitle";
+import axios from "axios";
+
+export default function MostRead() {
+  const [mostReadPosts, setMostReadPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function () {
+    const fetchPost = async () => {
+      try {
+        const response = await axios.get("posts-preview/type-2/");
+        setMostReadPosts(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.log("Error fetching posts:", error);
+      }
+    };
+    fetchPost();
+  }, []);
+
+  return (
+    <>
+      <div className="mt-5">
+        <ComponentTitle
+          title="Most Read (All Time)"
+          url="/discover?sort_by=most-read"
+        />
+        <div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-2">
+            {loading ? (
+              <>
+                <span className="text-gray-400">Loading...</span>
+              </>
+            ) : (
+              ""
+            )}
+            {mostReadPosts.map((post, index) => (
+              <MostReadPostElem
+                url={"/post/" + post.slug}
+                title={post.title}
+                author={post.author}
+                thumbnail={post.img_url}
+                date={post.published_at}
+                views={post.total_views}
+                key={index}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/shared/MostReadPostElem.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import formatDate from "../utils/formatDate";
+
+interface PropsType {
+  url: string;
+  title: string;
+  author: string;
+  thumbnail: string;
+  date: string;
+  views: number;
+}
+
+export default function MostReadPostElem({
+  url,
+  title,
+  author,
+  thumbnail,
+  date,
+  views,
+}: PropsType) {
+  const fDate = formatDate(date);
+  return (
+    <>
+      <Link href={url} className="w-full">
+        <div className="block md:flex gap-x-3 cursor-pointer duration-300 hover:opacity-70">
+          <div className="w-full md:w-1/3">
+            <div className="bg-blue-200 p-1 w-full h-[90px] rounded-lg relative">
+              <img
+                src={thumbnail}
+                alt={title}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
+          <div className="mt-2 md:mt-0 w-full md:w-2/3">
+            <div className="font-bold text-white">{title}</div>
+            <div className="text-gray-400 font-semibold">{author}</div>
+            <div className="block md:flex text-gray-400 gap-x-2 justify-between items-center">
+              <div>{fDate}</div>
+              <div className="hidden md:block">
+                <div className="w-1 h-1 bg-gray-400 rounded-full"></div>
+              </div>
+              <div>{views} Reader(s)</div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+}
