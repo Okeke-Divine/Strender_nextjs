9c07e357e74d57dc9b60ef7674195a282595c785
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-14 21:06:33 +0100

    Updated the view post route

--- src/app/api/post/[id]/route.ts
+++ src/app/api/post/[id]/route.ts
@@ -2,18 +2,28 @@ import prisma from "@/db";
 import { NextResponse } from "next/server";
 
 export const GET = async (req: Request, res: Response) => {
-  const post_id = 1;
+  try {
+    const { pathname } = new URL(req.url);
+    console.log(pathname);
 
-  const post = await prisma.post.findUnique({ where: { id: post_id } });
-  if (post) {
+    const id = pathname.substring("/api/post/".length); 
+
+    const post = await prisma.post.findUnique({ where: { id: id } });
+    if (post) {
+      return NextResponse.json(
+        { message: "Success", data: post },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { message: "Post does not exist" },
+        { status: 404 }
+      );
+    }
+  } catch (e) {
     return NextResponse.json(
-      { message: "Success", data: post },
-      { status: 200 }
-    );
-  } else {
-    return NextResponse.json(
-      { message: "Post does not exist" },
-      { status: 404 }
+      { message: "An error occured. Details: " + e },
+      { status: 500 }
     );
   }
 };
