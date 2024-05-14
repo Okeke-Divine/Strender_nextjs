3ba9bb8428107142b0bc1084149b3ab2a8ab6d73
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-14 12:07:53 +0100

    Updsted the tersma and condtions patge

--- src/app/t&c/page.tsx
+++ src/app/t&c/page.tsx
@@ -1,11 +1,28 @@
-export const metadata = {
-    title: "Terms and Conditions"
-}
+import TC from "@/data/T&C.json";
 
-export default function TermsAndConditions(){
-    return (
-        <>
-            <div>,e</div>
-        </>
-    )
+export const metadata = {
+  title: "Terms and Conditions",
+};
+
+export default function TermsAndConditions() {
+  return (
+    <>
+      <main className="mainComponent">
+        {TC.sections.map((section, index) => (
+          <div key={index} className="mb-8">
+            <h2 className="text-3xl font-bold text-white mb-4">
+              {section.title}
+            </h2>
+            <ul className="list-disc list-inside">
+              {section.content.map((item, idx) => (
+                <li key={idx} className="text-gray-400 mb-2">
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </main>
+    </>
+  );
 }
