a8540d5804ce580bf6b29e9c450ec870b7888017
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-14 20:11:25 +0100

    Updated

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,14 @@
 import { NextResponse } from "next/server"
 
+export const POST = async (req:Request,res:Response) => {
+    try{
+        const {title,categoryId,img_url,summary,content,tags} = await req.json();
+        const slug = "";
+    }catch(e){
+        NextResponse.json({message:e},{status:500})
+    }
+}
+
 export const GET = async (req:Request,res:Response) => {
-    return NextResponse.json({},{status:200})
+    return NextResponse.json({message:"Okay"},{status:200})
 }
