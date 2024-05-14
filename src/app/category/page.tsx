831f09a2d739e072430bcd9fc09882edbafa7ca9
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-14 21:40:58 +0100

    Updated

---
+++ src/app/category/page.tsx
@@ -0,0 +1,11 @@
+import Category from "@/components/shared/Category";
+
+export default function CategoryPage() {
+  return (
+    <>
+      <main className="mainComponent">
+        <Category />
+      </main>
+    </>
+  );
+}
