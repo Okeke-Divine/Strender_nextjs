7c22ce5d760b208970a3efe5ca167211698ae0e2
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-16 00:49:14 +0100

    Updated

--- src/app/post/[title]/page.tsx
+++ src/app/post/[title]/page.tsx
@@ -26,8 +26,8 @@ export default async function Post({ params }: { params: { title: string } }) {
       slug: slug,
     },
     include: {
-      category: true
-    }
+      category: true,
+    },
   });
 
   if (post) {
@@ -59,47 +59,48 @@ export default async function Post({ params }: { params: { title: string } }) {
       <main className="mainComponent2 mt-2">
         <div className="relative w-full h-[300px] rounded-3xl overflow-hidden shadow-md shadow-yellow-400">
           <img
-            src="/assets/images/banner/pexels-dom-le-roy-3991976.jpg"
+            // src="/assets/images/banner/pexels-dom-le-roy-3991976.jpg"
+            src={post.img_url}
             className="object-cover w-full h-full"
             alt="Football"
           />
-          {/* <div className="absolute inset-0 bg-black opacity-30"></div> */}
+          <div className="absolute inset-0 bg-black opacity-40"></div>
         </div>
       </main>
       <main className="mainComponent relative -top-20 mb-[-5rem]">
-            <div className="block md:flex bg-white rounded-xl shadow-lg p-5 gap-5">
-              <div className="w-full md:w-4/6">
-                {/* image */}
-                <div className="w-[6rem] h-[6rem] relative -top-[70px] mb-[-60px] rounded-full border-2 border-white">
-                  <img
-                    src={post.img_url}
-                    alt={post.title}
-                    className="w-full h-full object-cover rounded-full"
-                  />
-                </div>
-                {/* category */}
-                <div>
-                  <__Tag name={post.category.name} />
-                </div>
-                {/* post title */}
-                <div className="my-3 font-bold text-2xl">{post.title}</div>
-                {/* post author, date, views */}
-                <div className="flex items-center gap-x-3 font-semibold mt-1">
-                  <div>{post.author}</div>
-                  <div className="text-gray-500">{fDate}</div>
-                  <div>.</div>
-                  <div className="text-gray-500"> {post.total_views} views</div>
-                </div>
-                {/* post content */}
-                <div
-    className="text-gray-500 mt-3"
-    dangerouslySetInnerHTML={{ __html: post.content }}
-    style={{ whiteSpace: 'pre-wrap' }}
-  />
-                {/* google ad */}
-                <AdsComponent dataAdSlot="4045176239" />
-                {/* post tags */}
-                {/* <div className="mt-5">
+        <div className="block md:flex bg-white rounded-xl shadow-lg p-5 gap-5">
+          <div className="w-full md:w-4/6">
+            {/* image */}
+            <div className="w-[6rem] h-[6rem] relative -top-[70px] mb-[-60px] rounded-full border-2 border-white">
+              <img
+                src={post.img_url}
+                alt={post.title}
+                className="w-full h-full object-cover rounded-full"
+              />
+            </div>
+            {/* category */}
+            <div>
+              <__Tag name={post.category.name} />
+            </div>
+            {/* post title */}
+            <div className="my-3 font-bold text-2xl">{post.title}</div>
+            {/* post author, date, views */}
+            <div className="flex items-center gap-x-3 font-semibold mt-1">
+              <div>{post.author}</div>
+              <div className="text-gray-500">{fDate}</div>
+              <div>.</div>
+              <div className="text-gray-500"> {post.total_views} views</div>
+            </div>
+            {/* post content */}
+            <div
+              className="text-gray-500 mt-3"
+              dangerouslySetInnerHTML={{ __html: post.content }}
+              style={{ whiteSpace: "pre-wrap" }}
+            />
+            {/* google ad */}
+            <AdsComponent dataAdSlot="4045176239" />
+            {/* post tags */}
+            {/* <div className="mt-5">
                   <div className="font-bold mb-2">Tags:</div>
                   <div className="flex gap-2 flex-wrap">
                     {post.tags.split(",").map((tag, index) => (
@@ -107,14 +108,14 @@ export default async function Post({ params }: { params: { title: string } }) {
                     ))}
                   </div>
                 </div> */}
-                {/* comments */}
-                <Comment post_id={post.id} post_slug={post.slug} />
-              </div>
-              <div className="mt-5 md:mt-0 w-full md:w-2/6 border-l-2 h-fit p-5">
-                {/* <EmailSubscribe /> */}
-              </div>
-            </div>
-          </main>
+            {/* comments */}
+            <Comment post_id={post.id} post_slug={post.slug} />
+          </div>
+          <div className="mt-5 md:mt-0 w-full md:w-2/6 border-l-2 h-fit p-5">
+            {/* <EmailSubscribe /> */}
+          </div>
+        </div>
+      </main>
     </>
   );
 }
