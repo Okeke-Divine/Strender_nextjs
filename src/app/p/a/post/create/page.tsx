712e42c615930dbccc24a27e10ca9cbad0d67c38
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-16 00:25:21 +0100

    Updated

--- src/app/p/a/post/create/page.tsx
+++ src/app/p/a/post/create/page.tsx
@@ -3,10 +3,17 @@ import prisma from "@/db";
 export default async function CreatePost() {
   const categories = await prisma.category.findMany();
 
+    async function addPost(data:FormData){
+        "use server"
+        const title = data.get("title")?.toString()
+        const img_url = data.get("img_url")?.toString()
+        const categoryId = data.get("categoryId")?.toString()
+    }
+
   return (
     <>
       <div className="mainComponent text-white">
-        <form>
+        <form action={addPost}>
           <input required name="title" placeholder="Title" className="input2" />
           <br />
           <input
@@ -27,6 +34,8 @@ export default async function CreatePost() {
             ))}
           </select>
           <br />
+          <textarea placeholder="Summary" name="summary" className="input2" rows={2}></textarea><br />
+          <textarea placeholder="c  ontent" name="content" className="input2" rows={5}></textarea><br />
           <button>Add</button>
         </form>
       </div>
