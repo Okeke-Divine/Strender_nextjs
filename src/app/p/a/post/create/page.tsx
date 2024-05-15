a89218cdd5910db0c660bd00b8fc787391247997
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-16 00:12:17 +0100

    Updated

---
+++ src/app/p/a/post/create/page.tsx
@@ -0,0 +1,9 @@
+export default async function CreatePost(){
+    return (
+        <>
+            <div className="mainComponent text-white">
+                Lol
+            </div>
+        </>
+    )
+}
