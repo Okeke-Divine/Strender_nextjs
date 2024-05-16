c8a29792a0e72bdca6d9557956b040c51a704418
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-16 02:04:25 +0100

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import AdsterraAdComponent1 from "@/components/ads/adsterra/AdsterraAdComponent1";
 import PartialHero from "@/components/home/PartialHero";
 import Category from "@/components/shared/Category";
 import MostRead from "@/components/shared/MostRead";
@@ -6,6 +8,7 @@ export default function Home() {
   return (
     <>
       <PartialHero />
+      <AdsterraAdComponent1 />
       <main className="mainComponent">
         <Category />
         <MostRead />
---
+++ src/components/ads/adsterra/AdsterraAdComponent1.tsx
@@ -0,0 +1,7 @@
+export default function AdsterraAdComponent1() {
+  return (
+    <>
+      <div id="container-ffb1350a3987200d368c5f4b66815a32"></div>
+    </>
+  );
+}
