c72b7a7036cbb5377d22b062a011f9d1dbd1f05e
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-16 11:17:41 +0100

    Lol

---
+++ src/app/_document.tsx
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
+export default function Document() {
+    console.log('lol');
+    return (
+    <Html lang="en">
+      <Head>
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
