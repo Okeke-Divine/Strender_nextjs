c21e3a057a731c0928184ff03eb4e217ed634f76
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-16 22:20:50 +0100

    Updated the satic site

--- src/components/shared/MostRead.tsx
+++ src/components/shared/MostRead.tsx
@@ -1,25 +1,32 @@
-"use client";
-import { useState, useEffect } from "react";
+// "use client";
+// import { useState, useEffect } from "react";
 import ComponentTitle from "./ComponentTitle";
-import axios from "axios";
+import prisma from "@/db";
+// import axios from "axios";
 import MostReadPostElem from "./MostReadPostElem";
 
-export default function MostRead() {
-  const [mostReadPosts, setMostReadPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+export default async function MostRead() {
+  const mostReadPosts = await prisma.post.findMany({
+    orderBy: {
+      total_views: "desc",
+    },
+    take: 4,
+  });
+  // const [mostReadPosts, setMostReadPosts] = useState([]);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(function () {
-    const fetchPost = async () => {
-      try {
-        const response = await axios.get("/api/posts/most-read");
-        setMostReadPosts(response.data.data);
-        setLoading(false);
-      } catch (error) {
-        console.log("Error fetching posts:", error);
-      }
-    };
-    fetchPost();
-  }, []);
+  // useEffect(function () {
+  //   const fetchPost = async () => {
+  //     try {
+  //       const response = await axios.get("/api/posts/most-read");
+  //       setMostReadPosts(response.data.data);
+  //       setLoading(false);
+  //     } catch (error) {
+  //       console.log("Error fetching posts:", error);
+  //     }
+  //   };
+  //   fetchPost();
+  // }, []);
 
   return (
     <>
@@ -30,20 +37,20 @@ export default function MostRead() {
         />
         <div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-2">
-            {loading ? (
+            {/* {loading ? (
               <>
                 <span className="text-gray-400">Loading...</span>
               </>
             ) : (
               ""
-            )}
-            {mostReadPosts.map((post:any, index) => (
+            )} */}
+            {mostReadPosts.map((post: any, index: any) => (
               <MostReadPostElem
                 url={"/post/" + post.slug}
                 title={post.title}
                 author={post.author}
                 thumbnail={post.img_url}
-                date={post.published_at}
+                date={post.createdAtg}
                 views={post.total_views}
                 key={index}
               />
