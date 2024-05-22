dab3418c581cfefd67152d29216f09940afe0c4c
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-22 17:34:22 +0100

    Updated the dynamic sitemap

---
+++ src/app/dynamic-sitemap/categories/sitemap.ts
@@ -0,0 +1,31 @@
+import { MetadataRoute } from "next";
+import prisma from "@/db";
+import { NEXT_PUBLIC_BASE_URL } from "@/constants/shared/constant";
+
+type ChangeFrequency =
+  | "daily"
+  | "always"
+  | "hourly"
+  | "weekly"
+  | "monthly"
+  | "yearly"
+  | "never";
+
+const changeFrequency: ChangeFrequency = "daily";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const categories = await prisma.category.findMany({
+    select: {
+      name: true,
+      
+    },
+  });
+
+  const categories_posts = categories.map(({ name }) => ({
+    url: `${NEXT_PUBLIC_BASE_URL}/`+name,
+    lastModified: new Date().toISOString(),
+    changeFrequency: changeFrequency,
+  }));
+
+  return [...categories_posts];
+}
---
+++ src/app/dynamic-sitemap/posts/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+import prisma from "@/db";
+import { NEXT_PUBLIC_BASE_URL } from "@/constants/shared/constant";
+
+type ChangeFrequency =
+  | "daily"
+  | "always"
+  | "hourly"
+  | "weekly"
+  | "monthly"
+  | "yearly"
+  | "never";
+
+const changeFrequency: ChangeFrequency = "daily";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await prisma.post.findMany({
+    select: {
+      slug: true,
+    },
+  });
+
+  const sitemap_posts = posts.map(({ slug }) => ({
+    url: `${NEXT_PUBLIC_BASE_URL}/`+slug,
+    lastModified: new Date().toISOString(),
+    changeFrequency: changeFrequency,
+  }));
+
+  return [...sitemap_posts];
+}
