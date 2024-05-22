91accdd7527d1c949b7f70ee7da8caafb15f67c5
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-22 17:56:20 +0100

    Upated the site maps

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return {
-            source: "/api/(.*)",
-            headers: {
-                key: "Access-Control-Allow-Origin",
-                value: process.env.ALLOWED_ORIGIN,
-            }
-        }
-    }
+    // async headers() {
+    //     return {
+    //         source: "/api/(.*)",
+    //         headers: {
+    //             key: "Access-Control-Allow-Origin",
+    //             value: process.env.ALLOWED_ORIGIN,
+    //         }
+    //     }
+    // }
 };
 
 export default nextConfig;
