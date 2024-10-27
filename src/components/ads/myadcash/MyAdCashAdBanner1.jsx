b9b627685433b0439038b13cf4f3704d3ca8decc
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-10-27 11:54:12 +0100

    Updated

--- src/components/ads/myadcash/MyAdCashAdBanner1.jsx
+++ src/components/ads/myadcash/MyAdCashAdBanner1.jsx
@@ -5,15 +5,13 @@ import { useEffect, useRef } from 'react';
 export default function MyAdCashAdBanner1() {
     return (
         <>
-            <Script id="aclib" src="//acscdn.com/script/aclib.js" strategy="afterInteractive" />
+            {/* <Script id="aclib" src="//acscdn.com/script/aclib.js" strategy="afterInteractive" /> */}
             <div>
                 <Script id="ad-banner-script" strategy="afterInteractive">
                     {`
-                        if (typeof aclib !== 'undefined' && aclib.runBanner) {
                             aclib.runBanner({
                                 zoneId: '8940898',
                             });
-                        }
                     `}
                 </Script>
             </div>
