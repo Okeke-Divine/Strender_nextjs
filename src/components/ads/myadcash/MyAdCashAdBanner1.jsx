eeffa438df9b063234b3f7e566706401e057dcdd
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-10-27 12:08:40 +0100

    Updated

--- src/components/ads/myadcash/MyAdCashAdBanner1.jsx
+++ src/components/ads/myadcash/MyAdCashAdBanner1.jsx
@@ -7,15 +7,15 @@ export default function MyAdCashAdBanner1() {
             <div>
                 <Script id="ad-banner-script" strategy="afterInteractive">
                     {`
-                        // Block until aclib and runBanner are available
-                        while (typeof aclib === 'undefined' || typeof aclib.runBanner !== 'function') {
-                            // Empty loop to wait until aclib and aclib.runBanner are defined
-                        }
-
-                        // Once defined, execute the banner function
-                        aclib.runBanner({
-                            zoneId: '8940898',
-                        });
+                        (async function waitForAClib() {
+                            while (typeof aclib === 'undefined' || typeof aclib.runBanner !== 'function') {
+                                await new Promise(resolve => setTimeout(resolve, 1000)); // Wait 1 second before next check
+                            }
+                            // Once aclib and runBanner are available, execute the function
+                            aclib.runBanner({
+                                zoneId: '8940898',
+                            });
+                        })();
                     `}
                 </Script>
             </div>
